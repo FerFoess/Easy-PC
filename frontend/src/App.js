1cc1c518494863672ba69ad03899cf2654d8238e
Brayan7273 <brayangonzalez030405@gmail.com>
2024-10-24 14:55:26 -0600

    Auxilio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,8 @@ import Inicio from './Components/inicio/inicio';
 import Tipoequipo from './Components/filtrosPrevios/tipoequipo';
 import Propocito from './Components/filtrosPrevios/propocito';
 import Estadisticas from './Components/estadisticas/index'; 
-import Propocito from './Components/filtrosPrevios/propocito';
 import PropocitoSeleccion from './Components/seleccionComponentes/propocitoSeleccion';
+import CheckoutForm from './Components/procesoVenta/CheckoutForm';
 
 function App() {
   return (
@@ -22,6 +22,7 @@ function App() {
           <Route path="propocito" element={<Propocito />} />
           <Route path="estadisticas" element={<Estadisticas />} />
           <Route path="propocitoSeleccion" element={<PropocitoSeleccion/>} />
+          <Route path="checkoutForm" element={<CheckoutForm/>} />
         </Routes>
       </Router>
     </div>
