d244bf7a126e157ffdddcaaaa0d16e0f059dd0af
Braulio Salas <braulio.sg426@gmail.com>
2024-10-29 05:21:21 -0600

    Modificaciones

--- frontend/src/Components/estadisticas/index.jsx
+++ frontend/src/Components/estadisticas/index.jsx
@@ -1,87 +1,93 @@
 import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
-import 'chart.js/auto'; // Necesario para los gráficos de Chart.js
-import './css/styles.css'; // Importar el archivo CSS para el diseño
+import 'chart.js/auto';
+import './css/styles.css';
 
 const Ventas = () => {
   const [ventas, setVentas] = useState([]);
   const [idVentaBuscado, setIdVentaBuscado] = useState('');
   const [ventasFiltradas, setVentasFiltradas] = useState([]);
   const [totalDelRango, setTotalDelRango] = useState(0);
-  const [fechaInicio, setFechaInicio] = useState(new Date().toISOString().split('T')[0]); // Fecha actual por defecto para inicio
-  const [fechaFin, setFechaFin] = useState(new Date().toISOString().split('T')[0]); // Fecha actual por defecto para fin
+  const [fechaInicio, setFechaInicio] = useState(() => {
+    const fecha = new Date();
+    fecha.setDate(fecha.getDate() - 1); // Un día anterior
+    return fecha.toISOString().split('T')[0];
+  });
+  const [fechaFin, setFechaFin] = useState(() => {
+    const fecha = new Date();
+    fecha.setDate(fecha.getDate() - 1); // Un día anterior
+    return fecha.toISOString().split('T')[0];
+  });
   const [graficaDatos, setGraficaDatos] = useState({ labels: [], datasets: [] });
-  const [modoGrafica, setModoGrafica] = useState('diario'); // Estado para el tipo de gráfico: diario, semanal, mensual, anual
+  const [modoGrafica, setModoGrafica] = useState('diario');
   const [totalDelCorte, setTotalDelCorte] = useState(0);
+  const [paginaActual, setPaginaActual] = useState(1);
+  const registrosPorPagina = 10;
 
-  // Función para obtener todas las ventas del backend
   const obtenerVentas = async () => {
     try {
       const response = await fetch('http://localhost:3002/ventas/obtenerVentas');
       const data = await response.json();
-      setVentas(data);
-      setVentasFiltradas(data); // Inicialmente mostrar todas las ventas
-      calcularTotal(data); // Calcular el total de ventas para el rango de fechas inicial
-      actualizarGrafica(data); // Actualizar la gráfica con todas las ventas
+      const ventasOrdenadas = data.sort((a, b) => new Date(b.fecha) - new Date(a.fecha));
+
+      setVentas(ventasOrdenadas);
+      setVentasFiltradas(ventasOrdenadas);
+      calcularTotal(ventasOrdenadas);
+      actualizarGrafica(ventasOrdenadas);
+      calcularCorte(ventasOrdenadas);
     } catch (error) {
       console.error('Error al obtener las ventas:', error);
     }
   };
 
-  // Función para convertir la fecha a formato yyyy-mm-dd (ignorar la hora)
   const convertirASoloFecha = (fecha) => {
     const nuevaFecha = new Date(fecha);
-    return nuevaFecha.toISOString().split('T')[0]; // Devolver solo la parte de la fecha (yyyy-mm-dd)
+    return nuevaFecha.toISOString().split('T')[0];
   };
 
-  // Función para calcular el total de ventas basado en el rango de fechas
   const calcularTotal = (ventas) => {
     let total = 0;
     const inicio = convertirASoloFecha(fechaInicio);
     const fin = convertirASoloFecha(fechaFin);
 
     const ventasFiltradasPorFecha = ventas.filter(venta => {
-      const fechaVenta = convertirASoloFecha(venta.fecha); // Convertir fecha de la venta a yyyy-mm-dd
-      return fechaVenta >= inicio && fechaVenta <= fin; // Comparación solo con fechas, sin horas
+      const fechaVenta = convertirASoloFecha(venta.fecha);
+      return fechaVenta >= inicio && fechaVenta <= fin;
     });
 
     total = ventasFiltradasPorFecha.reduce((sum, venta) => sum + venta.costo * venta.cantidad, 0);
     setTotalDelRango(total);
   };
 
-  // Función para calcular el corte del día actual
-  const calcularCorte = () => {
-  const hoy = convertirASoloFecha(new Date()); // Obtener la fecha actual en formato yyyy-mm-dd
-  const ventasDeHoy = ventas.filter(venta => convertirASoloFecha(venta.fecha) === hoy); // Filtrar ventas solo de hoy
+  const calcularCorte = (ventas) => {
+    const ayer = new Date();
+    ayer.setDate(ayer.getDate() - 1); // Un día anterior
+    const ayerFecha = convertirASoloFecha(ayer);
 
-  const total = ventasDeHoy.reduce((sum, venta) => sum + venta.costo * venta.cantidad, 0); // Calcular el total
-  setTotalDelCorte(total); // Actualizar el estado con el total del corte
-};
+    const ventasDeAyer = ventas.filter(venta => convertirASoloFecha(venta.fecha) === ayerFecha);
 
+    const total = ventasDeAyer.reduce((sum, venta) => sum + venta.costo * venta.cantidad, 0);
+    setTotalDelCorte(total);
+  };
 
-  // Función para manejar el cambio en el campo de búsqueda
   const handleSearchChange = (e) => {
     const value = e.target.value;
     setIdVentaBuscado(value);
 
-    // Filtrar las ventas por el idVenta ingresado
     const filtered = ventas.filter(venta =>
-      venta.idVenta.toString().includes(value) // Comparación como cadena
+      venta.idVenta.toString().includes(value)
     );
     setVentasFiltradas(filtered);
   };
 
-  // Función para manejar el cambio de fecha inicial
   const handleFechaInicioChange = (e) => {
     setFechaInicio(e.target.value);
   };
 
-  // Función para manejar el cambio de fecha final
   const handleFechaFinChange = (e) => {
     setFechaFin(e.target.value);
   };
 
-  // Función para filtrar las ventas por el rango de fechas
   const filtrarPorRangoFechas = () => {
     const inicio = convertirASoloFecha(fechaInicio);
     const fin = convertirASoloFecha(fechaFin);
@@ -92,16 +98,15 @@ const Ventas = () => {
     }
 
     const ventasFiltradasPorFecha = ventas.filter(venta => {
-      const fechaVenta = convertirASoloFecha(venta.fecha); // Convertir fecha de la venta a yyyy-mm-dd
-      return fechaVenta >= inicio && fechaVenta <= fin; // Comparación solo con fechas
+      const fechaVenta = convertirASoloFecha(venta.fecha);
+      return fechaVenta >= inicio && fechaVenta <= fin;
     });
 
     setVentasFiltradas(ventasFiltradasPorFecha);
-    calcularTotal(ventasFiltradasPorFecha); // Calcular el total de las ventas filtradas
-    actualizarGrafica(ventasFiltradasPorFecha); // Actualizar la gráfica con las ventas filtradas
+    calcularTotal(ventasFiltradasPorFecha);
+    actualizarGrafica(ventasFiltradasPorFecha);
   };
 
-  // Función para obtener el número de la semana
   const getWeekNumber = (d) => {
     const date = new Date(d);
     const firstJanuary = new Date(date.getFullYear(), 0, 1);
@@ -109,7 +114,6 @@ const Ventas = () => {
     return Math.ceil((days + firstJanuary.getDay() + 1) / 7);
   };
 
-  // Función para actualizar los datos de la gráfica según el modo seleccionado
   const actualizarGrafica = (ventasFiltradasPorFecha) => {
     let labels = [];
     let data = [];
@@ -129,36 +133,33 @@ const Ventas = () => {
     const datosAgrupados = agruparPorFecha(ventasFiltradasPorFecha);
 
     if (modoGrafica === 'diario') {
-      labels = Object.keys(datosAgrupados).sort(); // Ordenar las fechas
-      data = labels.map(fecha => datosAgrupados[fecha]); // Obtener los valores según el orden de las etiquetas
+      labels = Object.keys(datosAgrupados).sort();
+      data = labels.map(fecha => datosAgrupados[fecha]);
     } else if (modoGrafica === 'semanal') {
-      // Agrupar por semanas
       const semanas = {};
       Object.entries(datosAgrupados).forEach(([fecha, ganancia]) => {
         const fechaObj = new Date(fecha);
-        const semana = `${fechaObj.getFullYear()}-W${getWeekNumber(fechaObj)}`; // Formato: año-semana
+        const semana = `${fechaObj.getFullYear()}-W${getWeekNumber(fechaObj)}`;
         if (!semanas[semana]) {
           semanas[semana] = 0;
         }
         semanas[semana] += ganancia;
       });
-      labels = Object.keys(semanas).sort(); // Ordenar las semanas
-      data = labels.map(semana => semanas[semana]); // Obtener los valores según el orden de las etiquetas
+      labels = Object.keys(semanas).sort();
+      data = labels.map(semana => semanas[semana]);
     } else if (modoGrafica === 'mensual') {
-      // Agrupar por meses
       const meses = {};
       Object.entries(datosAgrupados).forEach(([fecha, ganancia]) => {
         const fechaObj = new Date(fecha);
-        const mes = `${fechaObj.getFullYear()}-${fechaObj.getMonth() + 1}`; // Formato: año-mes
+        const mes = `${fechaObj.getFullYear()}-${fechaObj.getMonth() + 1}`;
         if (!meses[mes]) {
           meses[mes] = 0;
         }
         meses[mes] += ganancia;
       });
-      labels = Object.keys(meses).sort(); // Ordenar los meses
-      data = labels.map(mes => meses[mes]); // Obtener los valores según el orden de las etiquetas
+      labels = Object.keys(meses).sort();
+      data = labels.map(mes => meses[mes]);
     } else if (modoGrafica === 'anual') {
-      // Agrupar por años
       const anos = {};
       Object.entries(datosAgrupados).forEach(([fecha, ganancia]) => {
         const fechaObj = new Date(fecha);
@@ -168,8 +169,8 @@ const Ventas = () => {
         }
         anos[anio] += ganancia;
       });
-      labels = Object.keys(anos).sort(); // Ordenar los años
-      data = labels.map(anio => anos[anio]); // Obtener los valores según el orden de las etiquetas
+      labels = Object.keys(anos).sort();
+      data = labels.map(anio => anos[anio]);
     }
 
     setGraficaDatos({
@@ -186,7 +187,6 @@ const Ventas = () => {
     });
   };
 
-  // Hook para obtener las ventas al cargar el componente
   useEffect(() => {
     obtenerVentas();
   }, []);
@@ -194,12 +194,22 @@ const Ventas = () => {
   const handleModoGraficaChange = (e) => {
     const nuevoModo = e.target.value;
     setModoGrafica(nuevoModo);
-    actualizarGrafica(ventasFiltradas); // Actualizar la gráfica con el nuevo modo
+    actualizarGrafica(ventasFiltradas);
   };
-  
 
-return (
-  <div className='contenedor'>
+  const totalPaginas = Math.ceil(ventasFiltradas.length / registrosPorPagina);
+
+  const cambiarPagina = (numeroPagina) => {
+    setPaginaActual(numeroPagina);
+  };
+
+  const registrosAPresentar = ventasFiltradas.slice(
+    (paginaActual - 1) * registrosPorPagina,
+    paginaActual * registrosPorPagina
+  );
+
+  return (
+    <div className='contenedor'>
   <div className='rectangulo-gris'>
     <div className='columna-izquierda'>
   <h2>Ventas Filtradas</h2>
@@ -211,25 +221,36 @@ return (
     className="search-input"
   />
   <table className="ventas-table">
-    <thead>
-      <tr>
-        <th>ID Venta</th>
-        <th>Cantidad</th>
-        <th>Costo</th>
-        <th>Fecha</th>
-      </tr>
-    </thead>
-    <tbody>
-      {ventasFiltradas.map(venta => (
-        <tr key={venta.idVenta}>
-          <td>{venta.idVenta}</td>
-          <td>{venta.cantidad}</td>
-          <td>{venta.costo.toFixed(2)}</td>
-          <td>{convertirASoloFecha(venta.fecha)}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
+            <thead>
+              <tr>
+                <th>ID Venta</th>
+                <th>Cantidad</th>
+                <th>Costo</th>
+                <th>Fecha</th>
+              </tr>
+            </thead>
+            <tbody>
+              {registrosAPresentar.map(venta => (
+                <tr key={venta.idVenta}>
+                  <td>{venta.idVenta}</td>
+                  <td>{venta.cantidad}</td>
+                  <td>{venta.costo.toFixed(2)}</td>
+                  <td>{convertirASoloFecha(venta.fecha)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className="paginacion">
+            {Array.from({ length: totalPaginas }, (_, index) => (
+              <button
+                key={index + 1}
+                onClick={() => cambiarPagina(index + 1)}
+                className={`pagina-button ${paginaActual === index + 1 ? 'active' : ''}`}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
 </div>
 
 
@@ -248,6 +269,7 @@ return (
         </div>
       </div>
       <div className='fila-intermedia'>
+      <h3>Total de ganancias: ${totalDelRango.toFixed(2)} </h3>
         <input
           type="date"
           value={fechaInicio}
@@ -261,21 +283,17 @@ return (
           className='campof'
         />
         <button className='botoneslocos' onClick={filtrarPorRangoFechas}>Filtrar</button>
-        <h3>Total: {totalDelRango.toFixed(2)} </h3>
       </div>
 
       <div className='fila-inferior'>
       <h2>Corte</h2>
-      <h3>Total del corte: {totalDelCorte.toFixed(2)} </h3>
+      <h3>Total del Corte: ${totalDelCorte.toFixed(2)}</h3>
       <button className='botoneslocos' onClick={calcularCorte}>Realizar corte</button>
       </div>
     </div>
   </div>
   </div>
-);
-
+  );
 };
-
-
 
 export default Ventas;
